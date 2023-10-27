8f193004b7247ff93c1e9dcd1d426792962824a8
jrodriguez19 <jrodrigueznocua@gmail.com>
2023-10-27 22:44:22 +1100

    Dev settings modifed to use SQLite3

--- AuroraIOT/settings/dev.py
+++ AuroraIOT/settings/dev.py
@@ -7,14 +7,21 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "auroraiotdb",
-        "USER": config('DB_USERNAME_DEV'),
-        "PASSWORD": config('DB_PASSWORD_DEV'),
-        "HOST": config('DB_HOST_DEV'),
-        "PORT": config('DB_PORT_DEV'),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "sqlite3.db",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "auroraiotdb",
+#         "USER": config('DB_USERNAME_DEV'),
+#         "PASSWORD": config('DB_PASSWORD_DEV'),
+#         "HOST": config('DB_HOST_DEV'),
+#         "PORT": config('DB_PORT_DEV'),
+#     }
+# }
 
 MQTT_CLIENT_ID = "id-django-mqttclient-development"
 
