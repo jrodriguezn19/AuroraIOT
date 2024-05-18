1e08efd074886a9336dcd471da2d6b3a8f323c44
jrodriguez19 <jrodrigueznocua@gmail.com>
2024-05-18 19:48:01 +1000

    sensors template modified to query last 20 records only

--- sensors/views.py
+++ sensors/views.py
@@ -10,13 +10,15 @@ def sensors_homepage(request):
     # print(sensors.query)
     # print("\n")
 
-    data = Data_Received.objects.values("sensor_id",
-                                        "sensor_id__name",
-                                        "sensor_id__brand",
-                                        "sensor_id__type__type",
-                                        "sensor_id__location",
-                                        "data")
-    print(data)
-    print(data.query)
+    data = Data_Received.objects.values(
+        "time",
+        "sensor_id",
+        "sensor_id__name",
+        "sensor_id__brand",
+        "sensor_id__type__type",
+        "sensor_id__location",
+        "data",
+    ).order_by("-id")[:20]
+    # print(data.query)
     context = {"data": list(data)}
-    return render(request, 'sensors.html', context)
+    return render(request, "sensors.html", context)
