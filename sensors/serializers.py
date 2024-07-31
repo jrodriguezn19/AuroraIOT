d1625c346327c3e22a4b18d9acb9e6e8a42c1dcb
jrodriguez19 <jrodrigueznocua@gmail.com>
2024-07-31 17:44:25 +1000

    serializer comment removed

--- sensors/serializers.py
+++ sensors/serializers.py
@@ -2,14 +2,25 @@ from rest_framework import serializers
 from sensors.models import *
 from mqttclient.models import *
 
+
 class SensorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sensor
-        #fields = '__all__' #All fields shouldn't be exposed by default, expose just what is needed
-        fields = ['id','name', 'brand', 'location', 'type', 'sensor_configuration']
+        # fields = '__all__' #All fields shouldn't be exposed by default, expose just what is needed
+        fields = ["id", "name", "brand", "location", "type", "sensor_configuration"]
+
 
 class DataPZEM004tSerializer(serializers.ModelSerializer):
     class Meta:
         model = Data_PZEM004t
-        #fields = '__all__' #All fields shouldn't be exposed by default, expose just what is needed
-        fields = ['id', 'sensor_id', 'time', 'volts', 'amps', 'frequency', 'watts', 'energy', 'power_factor', ]
+        fields = [
+            "id",
+            "sensor_id",
+            "time",
+            "volts",
+            "amps",
+            "frequency",
+            "watts",
+            "energy",
+            "power_factor",
+        ]
