3cf1a17600d8c4b29e7ed7ed1f870c64d0c8fa71
jrodriguez19 <jrodrigueznocua@gmail.com>
2024-08-05 01:07:10 +1000

    gunicorn config modified

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,5 +4,9 @@ bind = "0.0.0.0:9000"
 #workers = 1
 workers = multiprocessing.cpu_count() * 2 + 1
 raw_env = ["DJANGO_SETTINGS_MODULE=AuroraIOT.settings.prod"]
+# Access log - records incoming HTTP requests
 accesslog = "./logs/gunicorn-access.log"
+# Error log - records Gunicorn server goings-on
 errorlog = "./logs/gunicorn-error.log"
+# How verbose the Gunicorn error logs should be 
+loglevel = "info"
