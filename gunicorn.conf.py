000db12e3a68be54c300289d16e032d12e718983
jrodriguez19 <jrodrigueznocua@gmail.com>
2024-08-05 22:39:17 +1000

    docker compose modified to create container in bridge network

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,8 @@
 import multiprocessing
 
 bind = "0.0.0.0:9000"
-#workers = 1
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
+#workers = multiprocessing.cpu_count() * 2 + 1
 raw_env = ["DJANGO_SETTINGS_MODULE=AuroraIOT.settings.prod"]
 # Access log - records incoming HTTP requests
 accesslog = "./logs/gunicorn-access.log"
