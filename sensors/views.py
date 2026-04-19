219e22ee0b61922fdf18a522619e2dad47b27bad
Jorge Rodriguez Nocua <jrodrigueznocua@gmail.com>
2026-04-19 12:43:06 +1000

    Remove comments

--- sensors/views.py
+++ sensors/views.py
@@ -25,31 +25,3 @@ class SensorDataViewSet(ReadOnlyModelViewSet):
             .filter(sensor_id=self.kwargs['sensor_pk'])
             .order_by('-time')
         )
-
-
-
-
-# from django.shortcuts import render
-# from .models import *
-# from mqttclient.models import *
-
-
-# def sensors_homepage(request):
-#     # sensors = Sensor.objects.values("name", "brand", "location", "type__type")
-#     # context = {"sensors": sensors}
-#     # print(sensors)
-#     # print(sensors.query)
-#     # print("\n")
-
-#     data = Data_Received.objects.values(
-#         "time",
-#         "sensor_id",
-#         "sensor_id__name",
-#         "sensor_id__brand",
-#         "sensor_id__type__type",
-#         "sensor_id__location",
-#         "data",
-#     ).order_by("-id")[:20]
-#     # print(data.query)
-#     context = {"data": list(data)}
-#     return render(request, "sensors.html", context)
